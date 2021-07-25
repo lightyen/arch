2a161a19ab455ac5cff43d119e318148f0f077ee
lightyen <lightyen0123@gmail.com>
2021-07-25 17:55:31 +0800

    update

--- scripts/bspwm.sh
+++ scripts/bspwm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pacman -S sddm-kcm xorg bspwm sxhkd picom dmenu nitrogen alacritty lxappearance nautilus file-roller
+sudo pacman -S sddm-kcm xorg bspwm sxhkd picom dmenu nitrogen alacritty lxappearance nautilus file-roller
 mkdir -p ~/.config/sxhkd
 mkdir -p ~/.config/bspwm
 curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/sxhkdrc -o ~/.config/sxhkd/sxhkdrc
