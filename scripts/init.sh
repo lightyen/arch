b87d9f334a777dc4f104318e7e02362ff69b295a
lightyen <lightyen0123@gmail.com>
2021-07-25 17:21:45 +0800

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sed -E -i "s/^#(ParallelDownloads.*)/\1/" /etc/pacman.conf
 pacstrap -i /mnt base linux linux-firmware base-devel vim sudo
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
