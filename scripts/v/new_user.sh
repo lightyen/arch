1948ac5c2b90cd5386eea9a89e4e9c0a63bb495e
lightyen <lightyen0123@gmail.com>
2021-07-26 19:43:30 +0800

    update

--- scripts/v/new_user.sh
+++ scripts/v/new_user.sh
@@ -2,8 +2,6 @@
 
 username=$1
 useradd -m $username
-su $username
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/scripts/v/user.sh)"
-exit
+runuser -l $username -c "$(curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/scripts/v/user.sh)"
 chsh --shell $(which zsh) $username
 passwd $username
