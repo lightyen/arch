9cf54cce9de787ec9c7755060582df63058a59ac
lightyen <lightyen0123@gmail.com>
2021-07-26 14:11:26 +0800

    update

--- scripts/bspwm.sh
+++ scripts/bspwm.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+mkdir -p $HOME/.config/alacritty
+curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/alacritty.yml -o $HOME/.config/alacritty/alacritty.yml
+
 mkdir -p $HOME/.config/sxhkd
-mkdir -p $HOME/.config/bspwm
 curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/sxhkdrc -o $HOME/.config/sxhkd/sxhkdrc
-curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/bspwmrc -o $HOME/.config/bspwm/bspwmrc
 chmod 644 $HOME/.config/sxhkd/sxhkdrc
+
+mkdir -p $HOME/.config/bspwm
+curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/bspwmrc -o $HOME/.config/bspwm/bspwmrc
 chmod 755 $HOME/.config/bspwm/bspwmrc
 
 curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/.xprofile -o $HOME/.xprofile
