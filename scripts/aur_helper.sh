8a34610f5c874ace636ae704e564f7f7712eaaa3
lightyen <lightyen0123@gmail.com>
2021-07-27 00:48:45 +0800

    update

--- scripts/aur_helper.sh
+++ scripts/aur_helper.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-
 cd /tmp
-if git clone "https://aur.archlinux.org/$1.git" -o $1; then
+git clone https://aur.archlinux.org/$1
+
+if [ -e $1/PKGBUILD ]; then
 	chown -R nobody $1
 	cd $1
-	sudo -u nobody makepkg 
-	yes "" | pacman -U *.pkg.tar.zst
+	sudo -u nobody makepkg -d
+	target=$(compgen -G "*.pkg.tar.*")
+	if [ -z target ]; then
+		echo ERROR: Package is not found. 1>&2
+		exit 1
+	fi
+	yes "" | pacman -U --asdeps $target
 	cd -
-	rm -rf $1
+else
+	echo ERROR: Not a valid package.
 fi
-cd -
