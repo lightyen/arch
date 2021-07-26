aca3484eae6ebdc97884a464f35a0808233e589a
lightyen <lightyen0123@gmail.com>
2021-07-26 18:21:49 +0800

    fix

--- scripts/user.sh
+++ scripts/user.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/scripts/user_any.sh $1)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/scripts/user_any.sh) $1"
 
 curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/.xprofile -o $HOME/.xprofile
