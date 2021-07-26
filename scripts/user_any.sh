a799b0e7f54ae7ccccdeb4c18d7332545b469883
lightyen <lightyen0123@gmail.com>
2021-07-26 18:37:59 +0800

    update

--- scripts/user_any.sh
+++ scripts/user_any.sh
@@ -6,7 +6,7 @@ echo "setup vim environment..."
 rm -rf ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 1>/dev/null 2>&1
 curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/.vimrc -o ~/.vimrc
-yes "" | vim -c 'PluginInstall' -c 'qa!' 1>/dev/null 2>&1
+echo "y" | vim -c 'PluginInstall' -c 'qa!' 1>/dev/null 2>&1
 
 ZSH=
 rm -rf $HOME/.oh-my-zsh
