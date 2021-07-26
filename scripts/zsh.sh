6cc373601adff117296837cdf2f939bbee069999
lightyen <lightyen0123@gmail.com>
2021-07-26 13:47:14 +0800

    update

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+# autoload -U compinit && compinit
 
+curl -fsSL "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/templates/zshrc.zsh-template" -o .zshrc
 yes "" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/1' $HOME/.zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-completions zsh-autosuggestions)/1' $HOME/.zshrc
-autoload -U compinit && compinit
 chsh -s $(which zsh)
