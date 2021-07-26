87c9353deae203a11cb64f14e048bacc01a576f2
lightyen <lightyen0123@gmail.com>
2021-07-26 18:14:25 +0800

    update

--- scripts/user_any.sh
+++ scripts/user_any.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+cd $HOME
+
 echo "setup vim environment..."
 rm -rf ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 1>/dev/null 2>&1
 curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/.vimrc -o ~/.vimrc
 yes "" | vim -c 'PluginInstall' -c 'qa!' 1>/dev/null 2>&1
 
+ZSH=
 curl -fsSL "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/templates/zshrc.zsh-template" -o .zshrc
 echo "y" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/1' $HOME/.zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-completions zsh-autosuggestions)/1' $HOME/.zshrc
-chsh -s $(which zsh)
+echo $1 | chsh -s $(which zsh)
 echo "autoload -U compinit && compinit" | zsh
 
 mkdir -p $HOME/.config/alacritty
