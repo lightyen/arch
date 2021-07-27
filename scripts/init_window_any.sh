86749100a1e28c53861bca536285b36bf703f8c7
lightyen <lightyen0123@gmail.com>
2021-07-27 12:26:30 +0800

    update

--- scripts/init_window_any.sh
+++ scripts/init_window_any.sh
@@ -5,23 +5,30 @@ pacman -S sddm-kcm xorg bspwm sxhkd picom dmenu nitrogen alacritty lxappearance 
 mkdir -p /usr/local/share/fonts
 
 curl -fsSL https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.32.14/sarasa-gothic-ttf-0.32.14.7z -o ./sarasa-gothic-ttf-0.32.14.7z
-mkdir -p /usr/local/share/fonts/sarasa
-7z x -o/usr/local/share/fonts/sarasa sarasa-gothic-ttf-0.32.14.7z
+if [ -e sarasa-gothic-ttf-0.32.14.7z ]; then
+	mkdir -p /usr/local/share/fonts/sarasa
+	7z x -o/usr/local/share/fonts/sarasa sarasa-gothic-ttf-0.32.14.7z
+	chmod 644 /usr/local/share/fonts/sarasa/*.ttf
+fi
 
-if curl -fsSL https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKtc-hinted.zip -o NotoSansCJKtc-hinted.zip; then
+curl -fsSL https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKtc-hinted.zip -o NotoSansCJKtc-hinted.zip
+if [ -e NotoSansCJKtc-hinted.zip ]; then
 	unzip NotoSansCJKtc-hinted.zip -d NotoSansCJKtc
 	mkdir -p /usr/local/share/fonts/NotoSansCJKtc
 	mv NotoSansCJKtc/*.otf /usr/local/share/fonts/NotoSansCJKtc
+	chmod 644 /usr/local/share/fonts/NotoSansCJKtc/*.otf
 	rm -rf NotoSansCJKtc NotoSansCJKtc-hinted.zip
 fi
 
-if curl -fsSL https://github.com/microsoft/cascadia-code/releases/download/v2106.17/CascadiaCode-2106.17.zip -o CascadiaCode-2106.17.zip; then
+curl -fsSL https://github.com/microsoft/cascadia-code/releases/download/v2106.17/CascadiaCode-2106.17.zip
+if [ -e CascadiaCode-2106.17.zip ]; then
 	unzip CascadiaCode-2106.17.zip -d CascadiaCode
 	mv CascadiaCode/ttf /usr/local/share/fonts/CascadiaCode
+	chmod 644 /usr/local/share/fonts/CascadiaCode/*.ttf
 	rm -rf CascadiaCode
 fi
 
-fc-cache -sf
+fc-cache -fv
 
 echo "GTK_IM_MODULE=fcitx" >> /etc/environment
 echo "QT_IM_MODULE=fcitx" >> /etc/environment
