605c9a9f42f6492755bb8c7d79f4657929558465
lightyen <lightyen0123@gmail.com>
2021-07-27 17:49:06 +0800

    update

--- scripts/init_user_any.sh
+++ scripts/init_user_any.sh
@@ -17,20 +17,9 @@ sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/1' $HOME/.zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-completions zsh-autosuggestions)/1' $HOME/.zshrc
 echo "autoload -U compinit && compinit" | zsh
 
-mkdir -p $HOME/.config/alacritty
-curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/alacritty.yml -o $HOME/.config/alacritty/alacritty.yml
+git clone https://github.com/lightyen/arch.git
+mkdir -p .config
+cp -r arch/user_config/* ./config
 
-mkdir -p $HOME/.config/sxhkd
-curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/sxhkdrc -o $HOME/.config/sxhkd/sxhkdrc
 chmod 644 $HOME/.config/sxhkd/sxhkdrc
-
-mkdir -p $HOME/.config/bspwm
-curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/bspwmrc -o $HOME/.config/bspwm/bspwmrc
 chmod 755 $HOME/.config/bspwm/bspwmrc
-
-fcitx
-sed -i 's/^#Font=Sans/Font=Noto Sans CJK TC/1' $HOME/.config/fcitx/conf/fcitx-classic-ui.config
-sed -i 's/^#MenuFont=Sans/MenuFont=Noto Sans Mono CJK TC/1' $HOME/.config/fcitx/conf/fcitx-classic-ui.config
-sed -i 's/^#FontLocale=zh_CN.UTF-8/FontLocale=zh_TW.UTF-8/1' $HOME/.config/fcitx/conf/fcitx-classic-ui.config
-sed -i 's/^#SkinType=default/SkinType=dark/1' $HOME/.config/fcitx/conf/fcitx-classic-ui.config
-sed -i 's/^#SelectionKey=1234567890/SelectionKey=asdfghjkl;/1' $HOME/.config/fcitx/conf/fcitx-classic-ui.config
