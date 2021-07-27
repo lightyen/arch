f404f3451586c1b6ad9183290f4b6c698a58701d
lightyen <lightyen0123@gmail.com>
2021-07-27 17:52:06 +0800

    fix config

--- scripts/init_user_any.sh
+++ scripts/init_user_any.sh
@@ -17,9 +17,9 @@ sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/1' $HOME/.zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-completions zsh-autosuggestions)/1' $HOME/.zshrc
 echo "autoload -U compinit && compinit" | zsh
 
-git clone https://github.com/lightyen/arch.git
-mkdir -p .config
-cp -r arch/user_config/* ./config
+git clone https://github.com/lightyen/arch.git -o $HOME/arch
+mkdir -p $HOME/.config
+cp -r $HOME/arch/user_config/* $HOME/.config
 
 chmod 644 $HOME/.config/sxhkd/sxhkdrc
 chmod 755 $HOME/.config/bspwm/bspwmrc
