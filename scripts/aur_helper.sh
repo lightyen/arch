fe301918d64c534ec793458245ec92f4a8f4ac33
lightyen <lightyen0123@gmail.com>
2021-07-28 10:09:53 +0800

    update

--- scripts/aur_helper.sh
+++ scripts/aur_helper.sh
@@ -5,7 +5,10 @@ git clone https://aur.archlinux.org/$1
 if [ -e $1/PKGBUILD ]; then
 	chown -R nobody $1
 	cd $1
-	sudo -u nobody makepkg -d
+	if sudo -u nobody makepkg; then
+	else
+		sudo -u nobody makepkg -d
+	fi
 	target=$(compgen -G "*.pkg.tar.*")
 	if [ -z target ]; then
 		echo ERROR: Package is not found. 1>&2
