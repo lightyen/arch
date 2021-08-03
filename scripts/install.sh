975202d575e152a59c150864308f52f38268955e
lightyen <lightyen0123@gmail.com>
2021-08-03 14:42:49 +0800

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -111,5 +111,5 @@ others
 mirrorlist
 sed -E -i "s/^#(ParallelDownloads.*)/\1/" /etc/pacman.conf
 yes "" | pacstrap -i /mnt base linux linux-firmware
-genfstab -U /mnt >>/mnt/etc/fstab
+genfstab -U /mnt >/mnt/etc/fstab
 arch-chroot /mnt
