d5ee7eeec62fa504cdb6ddde689b00059388103a
lightyen <lightyen0123@gmail.com>
2021-08-04 23:47:36 +0800

    update

--- scripts/_defaults.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ "yes" == "$(xdg-settings check default-web-browser microsoft-edge-beta.desktop)" ]; then
-	xdg-mime query default x-scheme-handler/https
-else
-	xdg-settings set default-web-browser microsoft-edge-beta.desktop
-fi
