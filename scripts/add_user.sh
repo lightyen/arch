f20a4b627713a3ce2e294c98263ac7984280b298
lightyen <lightyen0123@gmail.com>
2021-08-06 13:43:04 +0800

    update

--- scripts/add_user.sh
+++ scripts/add_user.sh
@@ -16,9 +16,9 @@ runuser -l $username -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/l
 if pacman -Q xorg-server 1>/dev/null 2>&1; then
 	runuser -l $username -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/scripts/init_user.sh)" -c "window"'
 	if dmesg | grep 'Hypervisor detected' 1>/dev/null; then
-		runuser -l $username -c "curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/vbox/.xprofile -o $HOME/.xprofile"
+		runuser -l $username -c "curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/vbox/.xprofile -o ~/.xprofile"
 	else
-		runuser -l $username -c "curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/.xprofile -o $HOME/.xprofile"
+		runuser -l $username -c "curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/.xprofile -o ~/.xprofile"
 	fi
 fi
 
