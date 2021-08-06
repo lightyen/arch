711955ffb9c1bb1215f833a486231e8fc05c32e6
lightyen <lightyen0123@gmail.com>
2021-08-06 13:41:25 +0800

    update

--- scripts/add_user.sh
+++ scripts/add_user.sh
@@ -5,7 +5,9 @@ set -e
 username=$1
 password=$2
 
-useradd -m $username
+if useradd -m $username -p $password; then
+	echo new user: $username
+fi
 
 init_user=$(curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/scripts/init_user.sh)
 runuser -l $username -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/scripts/init_user.sh)" -c "ohmyzsh"'
@@ -14,12 +16,10 @@ runuser -l $username -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/l
 if pacman -Q xorg-server 1>/dev/null 2>&1; then
 	runuser -l $username -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/scripts/init_user.sh)" -c "window"'
 	if dmesg | grep 'Hypervisor detected' 1>/dev/null; then
-		runuser -l $username -c "$(curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/vbox/.xprofile -o $HOME/.xprofile)"
+		runuser -l $username -c "curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/vbox/.xprofile -o $HOME/.xprofile"
 	else
-		runuser -l $username -c "$(curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/.xprofile -o $HOME/.xprofile)"
+		runuser -l $username -c "curl -fsSL https://raw.githubusercontent.com/lightyen/arch/main/.xprofile -o $HOME/.xprofile"
 	fi
 fi
 
 chsh --shell $(which zsh) $username
-
-echo -e "$password\n$password" | passwd $username 1>/dev/null
